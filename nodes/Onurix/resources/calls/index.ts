9262f8a5737d3f6ef1b9fa5c34d3f5d43f4c1a57
Jhoan Zambrano <alejo.ax1306@gmail.com>
2026-03-21 01:19:36 -0500

    refactor: update calls operations and send description for improved structure and clarity

--- nodes/Onurix/resources/calls/index.ts
+++ nodes/Onurix/resources/calls/index.ts
@@ -1,32 +1,37 @@
 import type { INodeProperties } from 'n8n-workflow';
+import { callsSendDescription } from './send';
 
 const showOnlyForCalls = {
-    resource: ['calls'],
-};  
+	resource: ['calls'],
+};
 
 export const callsOperations: INodeProperties[] = [
-    {
-        displayName: 'Operation',
-        name: 'operation',
-        type: 'options',
-        noDataExpression: true,
-        displayOptions: {
-            show: showOnlyForCalls,
-        },
-        options: [
-            {
-                name: 'Make Call',
-                value: 'makeCall',
-                description: 'Make a voice call',
-                action: 'Make a voice call',
-                routing: {
-                    request: {
-                        method: 'POST',
-                        url: '/calls/make',
-                    },
-                }
-            }
-        ],
-        default: 'makeCall',
-    }
-];  
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: showOnlyForCalls,
+		},
+		options: [
+			{
+				name: 'Make Call',
+				value: 'makeCall',
+				description: 'Make a voice call',
+				action: 'Make a voice call',
+				routing: {
+					request: {
+						headers: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+						},
+						method: 'POST',
+						url: '/calls/make',
+					},
+				},
+			},
+		],
+		default: 'makeCall',
+	},
+	...callsSendDescription,
+];
--- nodes/Onurix/resources/calls/send.ts
+++ nodes/Onurix/resources/calls/send.ts
@@ -1,47 +1,26 @@
 import type { INodeProperties } from 'n8n-workflow';
 
-
-const showOnlyForCallsSend = {
-    operation: ['calls'],
-    resource: ['sms'],
+const showOnlyForCallsMake = {
+	operation: ['makeCall'],
+	resource: ['calls'],
 };
 
 export const callsSendDescription: INodeProperties[] = [
-    {
-        displayName: 'Phone Number',
-        name: 'phoneNumber',
-        type: 'string',
-        default: '',
-        required: true,
-        description: 'Phone number to send the SMS to. Include country code, e.g., +1234567890',
-        displayOptions: {
-            show: showOnlyForCallsSend,
-        },
-        routing: {
-            send: {
-                type: 'body',
-                property: 'phone',
-            }
-        }
-    },
-    {
-        displayName: 'Message',
-        name: 'sms',
-        type: 'string',
-        typeOptions: {
-            maxLength: 800,
-        },
-        default: 'Este es un mensaje desde n8n',
-        required: true,
-        description: 'Content of the SMS message (max 800 characters)',
-        displayOptions: {
-            show: showOnlyForCallsSend,
-        },
-        routing: {
-            send: {
-                type: 'body',
-                property: 'sms',
-            }
-        }
-    }
-];      
+	{
+		displayName: 'Phone Number',
+		name: 'phoneNumber',
+		type: 'string',
+		default: '',
+		required: true,
+		description: 'Phone number to call. Include country code, e.g., +1234567890.',
+		displayOptions: {
+			show: showOnlyForCallsMake,
+		},
+		routing: {
+			send: {
+				type: 'body',
+				property: 'phone',
+			},
+		},
+	},
+];
